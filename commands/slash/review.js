09bec26b7fbe145d0d42b7f100268be9dca6516c
FoggyLobster <bentleytenges@gmail.com>
2026-06-29 18:43:24 -0500

    A

--- commands/slash/review.js
+++ commands/slash/review.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
-const db = require("../database/database");
+const db = require("../../db");
 
 module.exports = {
   data: new SlashCommandBuilder()
