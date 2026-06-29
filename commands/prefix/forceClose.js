c92bd19ff2ec71d270f3a7d4ade2b17a39f48ce3
FoggyLobster <bentleytenges@gmail.com>
2026-06-29 13:55:11 -0500

    A

--- commands/prefix/forceClose.js
+++ commands/prefix/forceClose.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "close",
   description: "Closes the ticket.",
-  execute(interaction) {
-    interaction.reply("Closing the ticket...");
+  execute(message) {
+    message.reply("Closing the ticket...");
     const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
     sleep(5000).then(() => {
       message.channel.delete();
