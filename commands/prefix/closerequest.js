259173017472f8f816c986a7918b1f3745ab5b63
FoggyLobster <bentleytenges@gmail.com>
2026-06-29 12:11:21 -0500

    A

---
+++ commands/prefix/closerequest.js
@@ -0,0 +1,67 @@
+module.exports = {
+  name: "cr",
+
+  async execute(message) {
+    const hasRole = message.member.roles.cache.has("1520836300461183169");
+    const isAdmin = message.member.permissions.has("Administrator");
+    if (!hasRole && !isAdmin) {
+      return;
+    }
+    if (
+      !message.channel.topic ||
+      !/^\d+(\|\d+)?$/.test(message.channel.topic)
+    ) {
+      return;
+    }
+
+    const [userId] = message.channel.name.split("-");
+    await message.delete();
+    await message.channel.send({
+      flags: 32768,
+      components: [
+        {
+          type: 17,
+          components: [
+            {
+              type: 10,
+              content: `# <:bell:1503469398676209820> Close Request\n<@${userId}>`,
+            },
+            {
+              type: 14,
+              spacing: 2,
+            },
+            {
+              type: 10,
+              content:
+                "Our team feels you do not need further assistance. If you do not need further assistance, please click the 'Close' button promptly. If you still need further assistance, feel free to click the 'Keep Open' button, and our team will assist you as soon as possible.",
+            },
+            {
+              type: 14,
+              divider: false,
+            },
+            {
+              type: 1,
+              components: [
+                {
+                  style: 4,
+                  type: 2,
+                  label: "Close",
+                  custom_id: "crclose",
+                },
+                {
+                  style: 3,
+                  type: 2,
+                  custom_id: "keep_open",
+                  flow: {
+                    actions: [],
+                  },
+                  label: "Keep Open",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+  },
+};
