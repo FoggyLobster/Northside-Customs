35eaa6f9e2d87e4c03536ba2062081a004f2d46e
FoggyLobster <bentleytenges@gmail.com>
2026-07-03 14:11:16 -0500

    A

---
+++ commands/prefix/remove.js
@@ -0,0 +1,48 @@
+const { ChannelType, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+  name: "remove",
+  description: "Remove someone from the ticket",
+
+  async execute(message, args) {
+    const hasRole = message.member.roles.cache.has("1520836300461183169");
+    const isAdmin = message.member.permissions.has("Administrator");
+
+    if (!hasRole && !isAdmin) return;
+
+    const user_id = message.split(" ")[1];
+
+    if (!user_id) {
+      return message.reply("Please provide a user ID.");
+    }
+
+    const user = await message.guild.members.fetch(user_id);
+
+    if (!user) {
+      return message.reply("User not found.");
+    }
+
+    permissionOverwrites = [
+      {
+        id: message.guild.roles.everyone.id,
+        deny: [PermissionFlagsBits.ViewChannel],
+      },
+      {
+        id: user.id,
+        allow: [
+          PermissionFlagsBits.ViewChannel,
+          PermissionFlagsBits.SendMessages,
+          PermissionFlagsBits.ReadMessageHistory,
+        ],
+      },
+      {
+        id: message.guild.members.me.id,
+        allow: [
+          PermissionFlagsBits.ViewChannel,
+          PermissionFlagsBits.SendMessages,
+          PermissionFlagsBits.ManageChannels,
+        ],
+      },
+    ];
+  },
+};
