079d64658ecf9376071828fce958c0b5e1f3cc62
FoggyLobster <bentleytenges@gmail.com>
2026-07-03 14:56:34 -0500

    A

--- commands/prefix/restart.js
+++ commands/prefix/restart.js
@@ -6,34 +6,20 @@ module.exports = {
   async execute(message) {
     if (!message.member.permissions.has("Administrator")) return;
 
-    await message.reply("Restarting **Northside**...");
+    const msg = await message.reply(
+      "Restarting & Syncing **Northside** with **<:github:1501630356346896594> Github.**",
+    );
 
     setTimeout(() => {
-      exec("pm2 restart Northside", (error) => {
+      exec("git pull && pm2 restart Northside", (error) => {
         if (error) {
-          console.error("PM2 restart failed:", error);
+          console.error("PM2 sync failed:", error);
         }
       });
     }, 1000);
 
-    const wantsSync = args[0];
-
-    if (wantsSync === "sync") {
-      const msg = await message.reply(
-        "Restarting & Syncing **Northside** with **<:github:1501630356346896594> Github.**",
-      );
-
-      setTimeout(() => {
-        exec("git pull && pm2 restart Northside", (error) => {
-          if (error) {
-            console.error("PM2 sync failed:", error);
-          }
-        });
-      }, 1000);
-
-      msg.edit(
-        "Restarted & Synced **Northside** with **<:github:1501630356346896594> Github.**",
-      );
-    }
+    msg.edit(
+      "Restarted & Synced **Northside** with **<:github:1501630356346896594> Github.**",
+    );
   },
 };
