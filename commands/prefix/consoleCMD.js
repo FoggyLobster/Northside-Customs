34978265a91f803544e066caebe69937410bf619
FoggyLobster <bentleytenges@gmail.com>
2026-07-03 16:10:40 -0500

    A

--- commands/prefix/consoleCMD.js
+++ commands/prefix/consoleCMD.js
@@ -1,6 +1,6 @@
 const { EmebdBuilder } = require("discord.js");
 const { exec } = require("child_process");
-const db = require("../../db");
+
 module.exports = {
   name: "console",
   description: "Executes a console command.",
@@ -9,17 +9,22 @@ module.exports = {
     if (!isOwner) {
       return message.reply("You do not have permission to use this command.");
     }
+
     const cmd = args.join(" ");
+
     exec(cmd, (error, stdout, stderr) => {
       if (error) {
         return message.reply(
           `Failed to execute command:\n\`\`\`\n${error.message}\n\`\`\``,
         );
       }
+
       const output = (stdout || stderr).trim();
+
       if (!output) {
         return message.reply("No output found.");
       }
+
       const embed = new EmbedBuilder()
         .setTitle("Console Command Output")
         .setColor(0xff0000)
