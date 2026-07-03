e159438f3c9ca3a55d9c83138996b53739b5b17d
FoggyLobster <bentleytenges@gmail.com>
2026-07-02 21:35:08 -0500

    A

---
+++ commands/prefix/recentErrors.js
@@ -0,0 +1,15 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "recentErrors",
+  description: "Shows the last 10 errors that have occured in the server",
+  execute(message) {
+    const errors = message.client.recentErrors;
+    if (!errors.length) return message.reply("No errors have occured yet.");
+    const embed = new EmbedBuilder()
+      .setTitle("Recent Errors")
+      .setColor("#FF0000")
+      .setDescription(errors.map((e, i) => `${i + 1}. ${e}`).join("\n"));
+    message.channel.send(embed);
+  },
+};
