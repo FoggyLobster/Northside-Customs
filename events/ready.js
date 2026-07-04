b6d22782363444a6b949926f980750df95918925
FoggyLobster <bentleytenges@gmail.com>
2026-07-04 10:10:47 -0500

    A

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,19 @@
+const { ActivityType, PresenceUpdateStatus } = require("discord.js");
+
 module.exports = {
   name: "ready",
   once: true,
   execute(client) {
     console.log(`Logged in as ${client.user.tag}!`);
+
+    client.user.setPresence({
+      status: PresenceUpdateStatus.Idle,
+      activities: [
+        {
+          name: "Northside Customs",
+          type: ActivityType.Watching,
+        },
+      ],
+    });
   },
 };
