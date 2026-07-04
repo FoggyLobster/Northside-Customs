013eee77e914bd839df2f6da872a427af62152b8
FoggyLobster <bentleytenges@gmail.com>
2026-07-03 20:46:34 -0500

    A

--- commands/prefix/status.js
+++ commands/prefix/status.js
@@ -1,21 +1,23 @@
 module.exports = {
   name: "status",
   description: "Check the status of the bot",
-  execute(message, args) {
+
+  async execute(message) {
     const isOwner = message.author.id === "1062166609931804702";
 
     if (!isOwner) {
       return;
     }
 
-    const responseTime = Math.round(
-      message.createdTimestamp - message.editedTimestamp,
-    );
+    const statusMessage = await message.channel.send("Loading...");
 
-    message.channel.send(`
-        **Uptime:** ${Math.round(process.uptime())} seconds
-        **Response time:** ${responseTime} milliseconds
-        **Memory usage:** ${Math.round(process.memoryUsage().rss / 1024 / 1024)} MB
-        `);
+    const responseTime =
+      statusMessage.createdTimestamp - message.createdTimestamp;
+
+    await statusMessage.edit(`
+**Uptime:** ${Math.round(process.uptime())} seconds
+**Response time:** ${responseTime} milliseconds
+**Memory usage:** ${Math.round(process.memoryUsage().rss / 1024 / 1024)} MB
+`);
   },
 };
