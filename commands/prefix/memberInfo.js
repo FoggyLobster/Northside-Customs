f5b39795af25438235bd4252502a6a0595d3d9b4
FoggyLobster <bentleytenges@gmail.com>
2026-07-05 15:56:18 -0500

    A

---
+++ commands/prefix/memberInfo.js
@@ -0,0 +1,39 @@
+module.exports = {
+  name: "memberinfo",
+  description: "Get info about a member",
+
+  async execute(message, args) {
+    const member_id = args[0];
+    const member = await message.guild.members.fetch(member_id);
+
+    if (!member) {
+      return message.reply("Member not found.");
+    }
+
+    if (member_id === message.author.id) {
+      const embed = new EmbedBuilder()
+        .setColor("Green")
+        .setTitle("Member Info")
+        .setDescription(
+          `**User:** ${member.user}\n**ID:** ${member.user.id}\n**Joined Server:** ${member.joinedAt}\n**Created At:** ${member.user.createdAt}\n**Roles:** ${member.roles.cache.map((role) => role.name).join(", ")}`,
+        )
+        .setTimestamp()
+        .setFooter({
+          text: `Requested by ${message.author.username}`,
+        });
+      message.reply({ embeds: [embed] });
+    } else {
+      const embed = new EmbedBuilder()
+        .setColor("Green")
+        .setTitle("Member Info")
+        .setDescription(
+          `**Username:** ${member.user.username}\n**Discriminator:** ${member.user.discriminator}\n**ID:** ${member.user.id}\n**Joined Server:** ${member.joinedAt}\n**Created At:** ${member.user.createdAt}\n**Bot:** ${member.user.bot}\n**Nickname:** ${member.nickname}\n**Roles:** ${member.roles.cache.map((role) => role.name).join(", ")}\n**Permissions:** ${member.permissions.toArray().join(", ")}`,
+        )
+        .setTimestamp()
+        .setFooter({
+          text: `Requested by ${message.author.username}`,
+        });
+      message.reply({ embeds: [embed] });
+    }
+  },
+};
