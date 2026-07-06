92545515e7e1513e632d980d3bb6b2110f845955
FoggyLobster <bentleytenges@gmail.com>
2026-07-06 09:43:53 -0500

    A

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -10,34 +10,34 @@ module.exports = {
 
     const channel = await member.guild.channels.fetch("1520788467351552190");
 
-    if (!channel?.isTextBased()) return;
-
     const memberCount = member.guild.memberCount;
 
-    await channel.send({
-      flags: 32768,
-      components: [
-        {
-          type: 10,
-          content: `Welcome, <@${member.id}> to **<:Northside:1520847420874031104> Northside Customs**! You are member \`#${memberCount}\`.`,
-        },
-        {
-          type: 1,
-          components: [
-            {
-              type: 2,
-              style: 2,
-              label: `${memberCount}`,
-              disabled: true,
-              emoji: {
-                id: "1523695035076448457",
-                name: "person",
-                animated: false,
+    if (channel) {
+      await channel.send({
+        flags: 32768,
+        components: [
+          {
+            type: 10,
+            content: `Welcome, <@${member.id}> to **<:Northside:1520847420874031104> Northside Customs**! You are member \`#${memberCount}\`.`,
+          },
+          {
+            type: 1,
+            components: [
+              {
+                type: 2,
+                style: 2,
+                label: `${memberCount}`,
+                disabled: true,
+                emoji: {
+                  id: "1523695035076448457",
+                  name: "person",
+                  animated: false,
+                },
               },
-            },
-          ],
-        },
-      ],
-    });
+            ],
+          },
+        ],
+      });
+    }
   },
 };
