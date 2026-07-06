87737b52bd77684c49881839aa5e8595a3d78bb1
FoggyLobster <bentleytenges@gmail.com>
2026-07-06 10:24:41 -0500

    A

--- db.js
+++ db.js
@@ -91,16 +91,18 @@ db.prepare(
   `,
 ).run();
 
+db.prepare("DELETE TABLE credits_history").run();
+
 db.prepare(
   `
   CREATE TABLE IF NOT EXISTS credits_history (
   user_id TEXT PRIMARY KEY,
   credits INTEGER NOT NULL,
   given_at INTEGER NOT NULL
-  credits_given = TEXT NOT NULL
+  given_credits = TEXT NOT NULL
   given_by TEXT NOT NULL,
   taken_at INTEGER NOT NULL
-  credits_taken = TEXT NOT NULL
+  taken_credits = TEXT NOT NULL
   taken_by TEXT NOT NULL
   )
   `,
