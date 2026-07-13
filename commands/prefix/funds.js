442dfbe109dbbf8eb105dd06bc2e4f4d3235ccb5
FoggyLobster <bentleytenges@gmail.com>
2026-07-12 22:13:35 -0500

    A

---
+++ commands/prefix/funds.js
@@ -0,0 +1,66 @@
+const axios = require("axios");
+const noblox = require("noblox.js");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "funds",
+  description: `Shows the roblox groups funds`,
+
+  async execute(message) {
+    if (message.author.bot) return;
+
+    const GROUP_ID = "573074243";
+    const ROBLOSECURITY = noblox.setCookie(process.env.ROBLOX_COOKIE);
+
+    const isAdmin = message.member.permissions.has("Administrator");
+
+    if (!isAdmin) {
+      return message.channel.send(
+        "You do not have permission to use this command.",
+      );
+    }
+
+    if (!ROBLOSECURITY && !GROUP_ID) {
+      return message.channel.send(
+        "Could not get group information. Please contact bot developer.",
+      );
+    }
+
+    const headers = {
+      Cookie: `.ROBLOSECURITY=${ROBLOSECURITY}`,
+    };
+
+    try {
+      const currentFunds = await axios.get(
+        `https://economy.roblox.com/v1/groups/${GROUP_ID}/currency`,
+        { headers },
+      );
+
+      let pendingFunds = 0;
+
+      try {
+        pendingFunds = await axios.get(
+          `https://economy.roblox.com/v1/groups/${GROUP_ID}/revenue/summary/Day`,
+          { headers },
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    } catch (err) {
+      console.log(err);
+      return message.channel.send("Something went wrong.");
+    }
+
+    const currentFunds = currentFundsRes.data.robux ?? 0;
+    const totalFunds = currentFunds + pendingFunds;
+
+    const embed = new EmbedBuilder()
+      .setTitle("Roblox Group Funds")
+      .setDescription(
+        `Current funds: ${currentFunds}\nPending funds: ${pendingFunds}\nTotal funds: ${totalFunds}`,
+      )
+      .setColor("Green");
+
+    await message.reply({ embeds: [embed] });
+  },
+};
