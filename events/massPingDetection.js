97ce7b32857ffd025c5100f19d4e779f4e58ff47
FoggyLobster <bentleytenges@gmail.com>
2026-07-14 19:51:46 -0500

    A

--- events/massPingDetection.js
+++ events/massPingDetection.js
@@ -17,7 +17,7 @@ const PING_LIMIT = 3;
 const pingCache = new Map();
 
 module.exports = {
-  name: Events.MessageCreate,
+  name: MessageCreate,
 
   async execute(message) {
     if (message.author.bot) return;
