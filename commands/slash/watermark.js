d8279b92ac3dd903fae2ab7c22756a977f535409
FoggyLobster <bentleytenges@gmail.com>
2026-07-17 12:22:22 -0500

    A

---
+++ commands/slash/watermark.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("watermark")
+    .setDescription("Add a watermark to an image.")
+    .addStringOption((option) =>
+      option
+        .setName("image")
+        .setDescription("The image to add the watermark to.")
+        .setRequired(true),
+    ),
+  async execute(interaction) {
+    const image = interaction.options.getString("image");
+    const WATERMARK_URL =
+      "https://cdn.discordapp.com/attachments/1520826464948322334/1527726832999206952/Dripzels_2D_Showcase_10_4.png?ex=6a5bb5fc&is=6a5a647c&hm=a0985ab305eaf5a22d3d4b941f1113c044e82ca4045fb877b652a44d3d7447ac&";
+
+    const finishedImage = `${image}?watermark=${WATERMARK_URL}`;
+  },
+};
