a06938f5c634cabc96dc9ba80616c1241deee52d
FoggyLobster <bentleytenges@gmail.com>
2026-07-17 12:22:46 -0500

    A

--- commands/slash/watermark.js
+++ commands/slash/watermark.js
@@ -16,5 +16,9 @@ module.exports = {
       "https://cdn.discordapp.com/attachments/1520826464948322334/1527726832999206952/Dripzels_2D_Showcase_10_4.png?ex=6a5bb5fc&is=6a5a647c&hm=a0985ab305eaf5a22d3d4b941f1113c044e82ca4045fb877b652a44d3d7447ac&";
 
     const finishedImage = `${image}?watermark=${WATERMARK_URL}`;
+
+    await interaction.reply({
+      content: finishedImage,
+    });
   },
 };
